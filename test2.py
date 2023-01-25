7b7399591276af9cf7419b90148744cf2c2da936
mdwgti26 <mdwgti16@epopcon.com>
2023-01-26 00:48:39 +0900

    test2.py 수정

--- test2.py
+++ test2.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import telegram
 from airflow import DAG
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 dag = DAG('hello_world', description='Hello World DAG',
@@ -15,14 +16,42 @@ def hello(**kwargs):
     print('Hello from {kw}'.format(kw=kwargs['my_keyword']))
 
 
-with DAG('hello_world', description='Hello World DAG',
-        schedule_interval='0 12 * * *',
-        start_date=datetime(2017, 3, 20), catchup=False) as dag:
+# with DAG('hello_world', description='Hello World DAG',
+#         schedule_interval='0 12 * * *',
+#         start_date=datetime(2017, 3, 20), catchup=False) as dag:
+#
+#     hello_operator = PythonOperator(task_id='hello_task',
+#                                     python_callable=hello,
+#                                     dag=dag,
+#                                     op_kwargs={'my_keyword': 'Airflow'}
+#                                     )
+#
+#     hello_operator
 
-    hello_operator = PythonOperator(task_id='hello_task',
+
+default_args = {
+    'start_date': datetime(2019, 1, 1),
+    'owner': 'Airflow',
+}
+
+
+# def process(p1):
+#     print(p1)
+#     return 'done'
+
+
+with DAG(dag_id='parallel_dag_test', schedule_interval='0 12 * * *', default_args=default_args, catchup=False) as dag:
+    # Tasks dynamically generated
+    tasks = [BashOperator(task_id='task_{0}'.format(t), bash_command='sleep 5'.format(t)) for t in range(1, 4)]
+
+    task_4 = PythonOperator(task_id='task_4',
                                     python_callable=hello,
                                     dag=dag,
                                     op_kwargs={'my_keyword': 'Airflow'}
                                     )
 
-    hello_operator
+    task_5 = BashOperator(task_id='task_5', bash_command='echo "pipeline done"')
+
+    task_6 = BashOperator(task_id='task_6', bash_command='sleep 5')
+
+    tasks >> task_4 >> task_5 >> task_6
