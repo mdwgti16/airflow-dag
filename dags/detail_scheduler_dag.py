e059ae4fd2abbaa23cecaf2cfd33e256d5f1880d
mdwgti26 <mdwgti16@epopcon.com>
2023-02-02 19:21:03 +0900

    site 변경

--- dags/detail_scheduler_dag.py
+++ dags/detail_scheduler_dag.py
@@ -14,13 +14,13 @@ default_args = {
 
 
 def process(p1):
-    cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=top.naverstore.com -Dmongodb.url=mongodb://acq:acq12345@10.98.192.100:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/airflow/files/application-0.0.1-SNAPSHOT.jar'
+    cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=lg.dns-shop.ru -Dmongodb.url=mongodb://acq:acq12345@10.98.192.100:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/airflow/files/application-0.0.1-SNAPSHOT.jar'
     print(cmd)
     subprocess.run(cmd.split(' '))
 
     return 'done'
 
-with DAG(dag_id='detail_scheduler', schedule_interval='0 0 * * *', default_args=default_args, catchup=False) as dag:
+with DAG(dag_id='detail_scheduler', schedule_interval='0 1 * * *', default_args=default_args, catchup=False) as dag:
 
     detail = PythonOperator(task_id='detail', python_callable=process, op_args=['my super parameter'])
     detail
