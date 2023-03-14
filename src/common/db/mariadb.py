f77cd700777c3f348726c8bf5888f2aaa7d4dd58
mdwgti26 <mdwgti16@epopcon.com>
2023-03-14 16:41:59 +0900

    dag 중간커밋

--- src/common/db/mariadb.py
+++ src/common/db/mariadb.py
@@ -1,7 +1,18 @@
+import pandas as pd
 from sqlalchemy import create_engine
+from sys import platform
 
 
 def acq_engine():
-    # return create_engine(f'mariadb+pymysql://acq:acq00!q@103.218.158.204:3306/acq', pool_recycle=3600)
-    return create_engine(f'mariadb+pymysql://acq:acq12345@10.103.220.109:3306/acq', pool_recycle=3600)
+    if platform == "linux" or platform == "linux2":
+        return create_engine(f'mariadb+pymysql://acq:acq12345@10.103.220.109:3306/acq', pool_recycle=3600)
+    else:
+        return create_engine(f'mariadb+pymysql://acq:acq00!q@103.218.158.204:3306/acq', pool_recycle=3600)
 
+
+def acq_service():
+    return pd.read_sql('SELECT * FROM SERVICE', acq_engine())
+
+
+def acq_interval():
+    return acq_service()['SCHEDULE_INTERVAL'].unique()
--- src/dag/detail_scheduler_dag.py
+++ src/dag/detail_scheduler_dag.py
@@ -1,67 +1,78 @@
-# import subprocess
-# from time import sleep
+import subprocess
+from time import sleep
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime, timedelta
+
+from src.common.db.mariadb import *
+
+
+def process(p1):
+    # cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=lg.dns-shop.ru -Dmongodb.url=mongodb://acq:acq12345@10.98.192.100:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/airflow/files/application-0.0.1-SNAPSHOT.jar'
+    # cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=top.naverstore.com -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar'
+    cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=lg.datart.cz -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar'
+    print(cmd)
+    subprocess.run(cmd.split(' '))
+
+    return 'done'
+
+
+# with DAG(dag_id='detail_scheduler', schedule_interval='0 1 * * *', default_args=default_args, catchup=False) as dag:
 #
-# from airflow import DAG
-# from airflow.operators.bash import BashOperator
-# from airflow.operators.empty import EmptyOperator
-# from airflow.operators.python import PythonOperator
-# from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-#
-#
-# from datetime import datetime, timedelta
-#
-# default_args = {
-#     # 'retries': 3,
-#     # 'retry_delay': timedelta(minutes=5),
-#     'start_date': datetime(2019, 1, 1),
-#     'owner': 'Airflow',
-# }
-#
-# def process(p1):
-#     # cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=lg.dns-shop.ru -Dmongodb.url=mongodb://acq:acq12345@10.98.192.100:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/airflow/files/application-0.0.1-SNAPSHOT.jar'
-#     # cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=top.naverstore.com -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar'
-#     cmd = 'java -DSpring.batch.job.names=DetailJob -Dacq.collectSite=lg.datart.cz -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar'
-#     print(cmd)
-#     subprocess.run(cmd.split(' '))
-#
-#     return 'done'
-#
-#
-# with DAG(dag_id='acq_detail_scheduler',
-#          schedule_interval='0 1 * * *',
-#          default_args=default_args,
-#          catchup=False,
-#          max_active_runs=1,
-#          on_failure_callback=None) as dag:
-#     start = EmptyOperator(task_id='start')
-#
-#     run = KubernetesPodOperator(
-#         task_id="acq-scheduler-java",
-#         namespace='default',
-#         image='test/image',
-#         secrets=[
-#             env
-#         ],
-#         image_pull_secrets=[k8s.V1LocalObjectReference('image_credential')],
-#         name="job",
-#         is_delete_operator_pod=True,
-#         get_logs=True,
-#         resources=pod_resources,
-#         env_from=configmaps,
-#         dag=dag,
-#     )
-#
-#     k = KubernetesPodOperator(
-#         name="hello-dry-run",
-#         image="debian",
-#         cmds=["bash", "-cx"],
-#         arguments=["echo", "10"],
-#         labels={"foo": "bar"},
-#         task_id="dry_run_demo",
-#         do_xcom_push=True,
-#     )
-#
-# k.dry_run()
 #     detail = PythonOperator(task_id='detail', python_callable=process, op_args=['my super parameter'])
 #
-#     start >> detail
+#     detail
+
+def airflow_failed_callback(context):
+    # message 작성
+    message = """
+            :red_circle: Task Failed.
+            *Dag*: {dag}
+            *Task*: {task}
+            *Execution Time*: {exec_date}
+            *Exception*: {exception}
+            *Log Url*: {log_url}
+            """.format(
+        dag=context.get('task_instance').dag_id,
+        task=context.get('task_instance').task_id,
+        exec_date=context.get('execution_time'),
+        exception=context.get('exception'),
+        log_url=context.get('task_instance').log_url
+    )
+    print('Airflow', message)
+
+
+def hello_world_py(dag_id):
+    print("This is DAG: {}".format(str(dag_id)))
+
+
+def create_dag(dag_id, interval, default_args):
+    dag = DAG(dag_id=dag_id,
+              schedule_interval=interval,
+              default_args=default_args,
+              catchup=False,
+              # max_active_runs=1,
+              on_failure_callback=airflow_failed_callback)
+
+    with dag:
+        t1 = PythonOperator(task_id="hello_world",
+                            python_callable=hello_world_py,
+                            op_kwargs={'dag_id': dag_id})
+    return dag
+
+
+for interval in acq_interval():
+    dag_name = 'ACQ_DETAIL_SCHEDULER'
+    dag_id = f'{dag_name}_{"_".join(interval)}'
+    default_args = {
+        # 'retries': 3,
+        # 'retry_delay': timedelta(minutes=5),
+        'start_date': datetime(2019, 1, 1),
+        'owner': 'Airflow',
+    }
+
+    globals()[dag_id] = create_dag(dag_id, interval, default_args)
