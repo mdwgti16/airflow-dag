b487a4b25729120fb06e8d885f49fe541c19fff0
mdwgti26 <mdwgti16@epopcon.com>
2023-03-14 16:48:08 +0900

    db password 변경

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM apache/airflow:2.5.1-python3.8
 
 RUN pip install --no-cache-dir --upgrade pip && \
-    pip install --no-cache-dir python-telegram-bot
+    pip install --no-cache-dir python-telegram-bot pymysql
--- src/common/db/mariadb.py
+++ src/common/db/mariadb.py
@@ -5,7 +5,7 @@ from sys import platform
 
 def acq_engine():
     if platform == "linux" or platform == "linux2":
-        return create_engine(f'mariadb+pymysql://acq:acq12345@10.103.220.109:3306/acq', pool_recycle=3600)
+        return create_engine(f'mariadb+pymysql://acq:acq00!q@10.103.220.109:3306/acq', pool_recycle=3600)
     else:
         return create_engine(f'mariadb+pymysql://acq:acq00!q@103.218.158.204:3306/acq', pool_recycle=3600)
 
