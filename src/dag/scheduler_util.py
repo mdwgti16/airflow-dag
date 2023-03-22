122da3ddbe8b2d9915c18ea03e81fe50ab7e8a30
mdwgti26 <mdwgti16@epopcon.com>
2023-03-22 16:55:21 +0900

    add Dlogging.config

--- src/dag/scheduler_util.py
+++ src/dag/scheduler_util.py
@@ -53,5 +53,9 @@ def create_task(r, job_names, start_time, end_time, log_file_prefix):
 
     return BashOperator(
         task_id=f"{collect_site}__{sub_site}__{get_cron_interval(r['SCHEDULE_INTERVAL'])}",
-        bash_command=f"java -DSpring.batch.job.names={job_names} -Dacq.collectSite={collect_site} -Dacq.subSite={sub_site} -Dacq.type={r.TYPE} -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -DlogFilePrefix={log_file_prefix} -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar {start_time} {end_time}"
+        bash_command=f"java -DSpring.batch.job.names={job_names} \
+        -Dacq.collectSite={collect_site} -Dacq.subSite={sub_site} -Dacq.type={r.TYPE} \
+        -Dmongodb.url=mongodb://acq:acq12345@10.98.30.157:27017/acq.acqlog?authSource=acq \
+        -Dmariadb.admin.url=jdbc:mariadb://10.103.220.109:3306/acq -DlogFilePrefix={log_file_prefix} \
+        -Dlogging.config=/opt/nfs/files/log4j2.xml -jar /opt/nfs/files/application-0.0.1-SNAPSHOT.jar {start_time} {end_time}"
     )
